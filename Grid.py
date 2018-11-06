03f7cb190e76a9de1cd9efc1f546f56b6b002881
YannGaucher <44807217+YannGaucher@users.noreply.github.com>
2018-11-06 17:51:12 +0100

    Update Grid.py

--- Grid.py
+++ Grid.py
@@ -14,7 +14,20 @@ class Grid:
                 angles[i,j] = 2*np.pi*rnd.random()
         self.angles = angles
     
-
+    def Energy(self) :
+        E = 0 
+        J =self.J
+        angles =self.angles
+        for i in range(L) :
+            for j in range(L) :
+                A = angles[i,j]
+                A1= angles[(i+1)%L,j]
+                A2 = angles[(i-1)%L,j]
+                A3 = angles[i,(j+1)%L]
+                A4 = angles[i,(j-1)%L]
+                E -= J/2*(np.cos(A-A1)+np.cos(A-A2)+np.cos(A-A2)+np.cos(A-A3)+np.cos(A-A4))
+        return E
+        
 def metropolis_move(config):
     """Modify (or not) a configuration with Metropolis algorithm"""
     
@@ -44,8 +57,20 @@ def metropolis_move(config):
 
 def Monte_carlo(config,)
         
-        
-        
+
+
+
+L=50
+Gridex = Grid(L,1,1)
+
+Angles = Gridex.angles
+U=np.cos(Angles)*5
+V=np.sin(Angles)*5
+
+fig1, ax1 = plt.subplots()
+ax1.set_title('XY spins')
+Q = ax1.quiver(U, V, units='width',minshaft = 2)
+plt.show()
         
         
         
