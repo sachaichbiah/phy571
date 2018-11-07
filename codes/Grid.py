7a0a3a05cdff8180d5460ca2e955e8d5a969fd08
Sacha <sacha.ichbiah@polytechnique.edu>
2018-11-07 21:47:29 +0100

    Première simulation

--- codes/Grid.py
+++ codes/Grid.py
@@ -5,14 +5,17 @@ import numpy.random as rnd
 class Grid:
 #petit commentaire    
     
-    def __init__(self,L,J,beta) :
+    def __init__(self,L=10,J=1,beta=1) :
         angles = np.zeros((L,L))
+        self.beta = beta
         self.size=L
         self.J=J
         for i in range(L) :
             for j in range(L) :
                 angles[i,j] = 2*np.pi*rnd.random()
         self.angles = angles
+        self.energy = 0 
+        self.magnetization = 0
     
     def Energy(self) :
         E = 0 
@@ -27,7 +30,20 @@ class Grid:
                 A4 = angles[i,(j-1)%L]
                 E -= J/2*(np.cos(A-A1)+np.cos(A-A2)+np.cos(A-A2)+np.cos(A-A3)+np.cos(A-A4))
         return E
+     
         
+    
+def printGrid(config): 
+    #L=config.size
+    Angles=config.angles
+    U=np.cos(Angles)*5
+    V=np.sin(Angles)*5
+    fig1, ax1 = plt.subplots()
+    ax1.set_title('XY spins')
+    Q = ax1.quiver(U, V, units='width',minshaft = 2)
+    plt.show()     
+    
+    
 def metropolis_move(config):
     """Modify (or not) a configuration with Metropolis algorithm"""
     
@@ -48,18 +64,43 @@ def metropolis_move(config):
     
     # accept modification with Metropolis probability
     # if not accepted: leave configuration unchanged
-    if rnd.random() < np.exp(-beta*delta_energy):
+    if (rnd.random() < np.exp(-beta*delta_energy)):
         config.angles[i,j]=tetaNew
         config.energy += delta_energy
-        config.magnetization += np.add(np.array([np.cos(tetaNew),np.sin(tetaNew)]),np.array([np.cos(tetaOld),np.sin(tetaOld)])*(-1)
-     
+        config.magnetization += np.add(np.array([np.cos(tetaNew),np.sin(tetaNew)]),np.array([np.cos(tetaOld),np.sin(tetaOld)])*(-1))
         
-
-def Monte_carlo(config,)
         
+def Monte_carlo(config, length_cycle,n_cycles = 10000, n_warmup =1000): 
+    av_m, av_m2, av_e, av_e2 = 0,0,0,0
+    for i in range (n_warmup): 
+        metropolis_move(config)
+        
+        
+    for i in range(n_cycles) : 
+        for j in range (length_cycle) : 
+            metropolis_move(config)
+        
+        av_m+=config.magnetization
+        av_m2+=config.magnetization**2
+        av_e+=config.energy
+        av_e2+=config.energy**2
+    
+    av_m/=n_cycles
+    av_m2/=n_cycles
+    av_e/=n_cycles
+    av_e2/=n_cycles
+                
+    """
+    
+A=Grid()
+printGrid(A)
+L=A.size
+Monte_carlo(A,L)
+printGrid(A)
 
+"""
 
-
+"""
 L=50
 Gridex = Grid(L,1,1)
 
@@ -70,8 +111,9 @@ V=np.sin(Angles)*5
 fig1, ax1 = plt.subplots()
 ax1.set_title('XY spins')
 Q = ax1.quiver(U, V, units='width',minshaft = 2)
-plt.show()
+plt.show()      
+     """
         
-        
-        
-        
+    
+    
+    
